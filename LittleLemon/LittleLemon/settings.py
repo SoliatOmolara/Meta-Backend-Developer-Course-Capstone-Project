276fed0ef899ccdd67ca5d80efcbfe2dbdb636d0
SoliatOmolara <azeezsoleehah@gmail.com>
2023-10-24 12:36:36 +0100

    Removed secret keys

--- LittleLemon/LittleLemon/settings.py
+++ LittleLemon/LittleLemon/settings.py
@@ -18,10 +18,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+from decouple import config
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--8)d_@3j+*xa$$vw8k)r6ix)uwj6qdgv1ht+x$j4vv=g5g$n%1'
-
+SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
