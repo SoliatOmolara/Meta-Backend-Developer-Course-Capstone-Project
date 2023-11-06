2f0800ed418736d0da3d8f468e4ae7c6fd081802
SoliatOmolara <azeezsoleehah@gmail.com>
2023-11-06 22:20:28 +0100

    Updating the Models

--- LittleLemon/restaurant/models.py
+++ LittleLemon/restaurant/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    Name = models.CharField(max_length=255)
+    No_of_guest = models.IntegerField()
+    BookingDate = models.DateTimeField()
+    
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    inventory = models.IntegerField()
