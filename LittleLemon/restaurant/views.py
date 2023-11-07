0f6d1bbbc56eab6c2d4768190095fd9c8521c2ea
SoliatOmolara <azeezsoleehah@gmail.com>
2023-11-07 11:32:06 +0100

    Updating the MenuApi

--- LittleLemon/restaurant/views.py
+++ LittleLemon/restaurant/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from rest_framework import generics
+
+from .models import Menu, Booking
+from .serializers import MenuSerializers
+
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializers
+    
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializers
